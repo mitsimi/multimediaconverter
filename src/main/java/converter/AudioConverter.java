7042da25490ee21a38d0878ef75e5e493ce8244a
Wesellon <fabian.wesenauer@outlook.com>
2023-05-19 22:32:43 +0200

    Implemented AudioConverter

--- src/main/java/converter/AudioConverter.java
+++ src/main/java/converter/AudioConverter.java
@@ -1,21 +1,39 @@
 package converter;
 
+import types.AudioType;
 import types.MediaType;
 
-import java.io.File;
-import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.*;
 
-// TODO - apply changes from Converter
 public class AudioConverter implements Converter {
 
-    public AudioConverter(File file) {
+    private byte[] fileData = null;
+    AudioType convertTo = null;
+
+    public AudioConverter(File file) throws IOException {
+        try (FileInputStream fis = new FileInputStream(file.getAbsoluteFile())) {
+            fileData = fis.readAllBytes();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void convert(String to_type) throws IOException {
+        convertTo = AudioType.getEnum(to_type.toLowerCase());
     }
 
     @Override
     public void save(String absolutePath, String fileName) {
+        System.out.println(absolutePath + "/" + fileName + "." + convertTo.fileExtension);
+        try (FileOutputStream fos = new FileOutputStream(absolutePath + "/" + fileName + "." + convertTo.fileExtension)) {
+            fos.write(fileData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
