f3cfed0ce195b9a9a06c6ff85efe5b9910fccdd6
Noah Füreder <noah.fuereder@liwest.at>
2023-05-26 10:16:48 +0200

    Added comments ImageConverter

--- src/main/java/converter/ImageConverter.java
+++ src/main/java/converter/ImageConverter.java
@@ -11,15 +11,28 @@ public class ImageConverter implements Converter {
     private final BufferedImage image;
     private ImageType type;
 
+    /**
+     * @param path to original image
+     * @throws IOException
+     */
     public ImageConverter(Path path) throws IOException {
         image = ImageIO.read(path.toFile());
     }
 
+    /**
+     * @param to_type type to which the image have to be covnerted
+     */
     @Override
     public void convert(String to_type) {
         type = ImageType.valueOf(to_type);
     }
 
+    /**
+     * Saves the converted image into the specified path with specified file name.
+     * @param absolutePath where the new image should be saved
+     * @param fileName naming of the file WITHOUT extension
+     * @throws IOException
+     */
     @Override
     public void save(String absolutePath, String fileName) throws IOException {
         Path path = Path.of(absolutePath, fileName + "." + type.getFileExtension());
