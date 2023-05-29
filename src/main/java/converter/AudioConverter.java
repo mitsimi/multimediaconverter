2789b08ccc07174c6c46e5153dfd71fdbe57c9c5
Wesellon <fabian.wesenauer@outlook.com>
2023-05-29 14:46:34 +0200

    AudioConverter converts formats properly now / added function for changing format settings

--- src/main/java/converter/AudioConverter.java
+++ src/main/java/converter/AudioConverter.java
@@ -1,43 +1,71 @@
 package converter;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import types.AudioType;
 import types.MediaType;
+import ws.schild.jave.Encoder;
+import ws.schild.jave.EncoderException;
+import ws.schild.jave.MultimediaObject;
+import ws.schild.jave.encode.AudioAttributes;
+import ws.schild.jave.encode.EncodingAttributes;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 import java.io.*;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class AudioConverter implements Converter {
-    
-    private byte[] fileData = null;
-    AudioType convertTo = null;
 
+    private AudioType convertTo = null;
+    private AudioAttributes audioAttributes = new AudioAttributes();
+    private EncodingAttributes encodingAttributes = null;
+    private MultimediaObject mmsObject = null;
+
+    //Unsupported meaning they cant be decoded by JAVE
+    private String[] unsupportedAudioFormats = {"aac","ogg","m4a"};
 
     public AudioConverter(Path path) {
-        try (FileInputStream fis = new FileInputStream(path.toFile())) {
-            fileData = fis.readAllBytes();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        mmsObject = new MultimediaObject(path.toFile());
     }
 
     @Override
     public void convert(String to_type) throws IOException {
         convertTo = AudioType.getEnum(to_type.toLowerCase());
+
+        encodingAttributes = new EncodingAttributes();
+        encodingAttributes.setOutputFormat(to_type.toLowerCase());
+        encodingAttributes.setAudioAttributes(audioAttributes);
     }
 
     @Override
     public void save(String absolutePath, String fileName) {
-        System.out.println(absolutePath + "/" + fileName + "." + convertTo.fileExtension);
-        try (FileOutputStream fos = new FileOutputStream(absolutePath + "/" + fileName + "." + convertTo.fileExtension)) {
-            fos.write(fileData);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Path path = Path.of(absolutePath + "/" + fileName + "." + convertTo.fileExtension);
+        System.out.println(path);
+        Encoder encoder = new Encoder();
+        try {
+            if (!Arrays.stream(unsupportedAudioFormats).toList().contains(convertTo.fileExtension)) {
+                encoder.encode(mmsObject, path.toFile(), encodingAttributes);
+            } else {
+                // Just changing file-endings
+                byte[] fileContent = Files.readAllBytes(mmsObject.getFile().toPath());
+                Files.write(path, fileContent, StandardOpenOption.CREATE);
+            }
+        } catch (EncoderException | IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public void setAudioAttributes(int bitrate, int channels, int samplingRate) {
+        this.audioAttributes.setBitRate(bitrate);
+        this.audioAttributes.setChannels(channels);
+        this.audioAttributes.setSamplingRate(samplingRate);
+    }
+
+    public void setAudioAttributes(AudioAttributes audioAttributes) {
+        this.audioAttributes = audioAttributes;
     }
 }
