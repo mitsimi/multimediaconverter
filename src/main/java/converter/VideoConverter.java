0f58441f95487cd688a4f9cb379a12617868ad80
Wesellon <fabian.wesenauer@outlook.com>
2023-06-03 23:42:48 +0200

    All filetypes are properly converting now (Audio/Video-converters)

--- src/main/java/converter/VideoConverter.java
+++ src/main/java/converter/VideoConverter.java
@@ -1,11 +1,5 @@
 package converter;
 
-import net.bramp.ffmpeg.FFmpeg;
-import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
-import net.bramp.ffmpeg.builder.FFmpegBuilder;
-import types.AudioType;
-import types.MediaType;
 import types.VideoType;
 import ws.schild.jave.Encoder;
 import ws.schild.jave.EncoderException;
@@ -18,39 +12,44 @@ import ws.schild.jave.info.VideoInfo;
 import ws.schild.jave.info.VideoSize;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 
 public class VideoConverter implements Converter {
     private VideoType convertTo = null;
-    private String pathFrom;
-    private String pathTo;
     private VideoType convertFrom = null;
+    private Path sourceFile = null;
+
     private VideoAttributes videoAttributes = new VideoAttributes();
     private AudioAttributes audioAttributes = new AudioAttributes();
     private EncodingAttributes encodingAttributes = null;
     private MultimediaObject mmsObject = null;
 
-    //Unsupported meaning they cant be decoded by JAVE
+    //Unsupported meaning they cant be encoded by the standard JAVE-Encoder
     private String[] unsupportedVideoFormats = {"wmv", "mkv", "mpg"};
 
+    /**
+     * Initializes the video-converter
+     *
+     * @param path The path-object of the source-file
+     */
     public VideoConverter(Path path) {
         mmsObject = new MultimediaObject(path.toFile());
         setStandardParams();
         convertFrom = VideoType.getEnum(path.toString().substring(path.toString().indexOf('.') + 1).toLowerCase());
+        sourceFile = path;
 
-        pathFrom = path.toAbsolutePath().toString();
         //Test Settings
-        //setVideoAttributes(4000000, 10, 0, new VideoSize(300, 480));
+        //setVideoAttributes(4000000, 10, new VideoSize(300, 480));
 
         //setAudioAttributes(...);
     }
 
+    /**
+     * Sets the audio- and video-attributes for the encoding
+     *
+     * @param to_type The type in which the source-file should get encoded in
+     */
     @Override
     public void convert(String to_type) {
         convertTo = VideoType.getEnum(to_type.toLowerCase());
@@ -60,16 +59,23 @@ public class VideoConverter implements Converter {
         encodingAttributes.setVideoAttributes(videoAttributes);
     }
 
+    /**
+     * Encodes the files and saves them.
+     *
+     * @param absolutePath The absolute Path of the destination-file
+     * @param fileName     The filename of the destination-file (without file-extension)
+     */
     @Override
     public void save(String absolutePath, String fileName) {
-        boolean fileTypesEqual = (convertTo == convertFrom);
+        Path destinPath = Path.of(absolutePath + fileName + "." + convertTo.fileExtension);
+        boolean fileTypesEqual = (sourceFile.toFile().getAbsolutePath().equals(destinPath.toFile().getAbsolutePath()));
 
         String tempFileName = fileName;
         if (fileTypesEqual) {
+            // this creates a tempFile with the files hashcode, because the source-file cannot be overwritten while encoding
             tempFileName += fileName.hashCode();
         }
         Path path = Path.of(absolutePath + "/" + tempFileName + "." + convertTo.fileExtension);
-        System.out.println(path);
         Encoder encoder = new Encoder();
         try {
             if (!Arrays.stream(unsupportedVideoFormats).toList().contains(convertTo.fileExtension)) {
@@ -93,16 +99,13 @@ public class VideoConverter implements Converter {
                         encoder.encode(mmsObject, path.toFile(), encodingAttributes);
                     }
                 }
-
-                if (fileTypesEqual) {
-                    File hashFile = path.toFile();
-                    File destFile = new File(absolutePath + "/" + fileName + "." + convertTo.fileExtension);
-                    System.out.println(hashFile.getAbsolutePath() + " " + destFile.getAbsolutePath());
-                    destFile.delete();
-                    hashFile.renameTo(destFile);
-                }
-
-
+            }
+            if (fileTypesEqual) {
+                File hashFile = path.toFile();
+                File destFile = destinPath.toFile();
+                System.out.println(hashFile.getAbsolutePath() + " " + destFile.getAbsolutePath());
+                destFile.delete();
+                hashFile.renameTo(destFile);
             }
         } catch (EncoderException e) {
             throw new RuntimeException(e);
@@ -110,26 +113,32 @@ public class VideoConverter implements Converter {
     }
 
     /**
-     * Sets the Video-attributes with the given settings.
+     * Sets the video-attributes with the given settings.
      *
      * @param bitrate    The bitrate of the Video (bit/s)
      * @param fps        The FPS of the Video
-     * @param quality    The quality of the Video (1 - 100)
      * @param resolution The resolution-Object of the Video
      */
-    public void setVideoAttributes(int bitrate, int fps, int quality, VideoSize resolution) {
+    public void setVideoAttributes(int bitrate, int fps, VideoSize resolution) {
         this.videoAttributes.setBitRate(bitrate);
         this.videoAttributes.setFrameRate(fps);
-        this.videoAttributes.setQuality(quality);
         this.videoAttributes.setSize(resolution);
     }
 
+    /**
+     * Sets the audio-attributes with the given settings.
+     *
+     * @param bitrate      The bitrate of the Audio (bit/s)
+     * @param channels     The channels of the Audio (1=Mono, 2=Stereo)
+     * @param samplingRate The sampling-rate of the Audio (Hz)
+     */
     public void setAudioAttributes(int bitrate, int channels, int samplingRate) {
         this.audioAttributes.setBitRate(bitrate);
         this.audioAttributes.setChannels(channels);
         this.audioAttributes.setSamplingRate(samplingRate);
     }
 
+    // Sets the encoding parameters to the same as the source-file if they are readable
     private void setStandardParams() {
         try {
             VideoInfo vi = mmsObject.getInfo().getVideo();
