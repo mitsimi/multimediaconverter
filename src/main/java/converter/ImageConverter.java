cdc90b195255b348f7b35116c055d9b3d0a5efa7
Noah Füreder <noah.fuereder@gmail.com>
2023-06-08 11:40:51 +0200

    fix: bmp conversion

--- src/main/java/converter/ImageConverter.java
+++ src/main/java/converter/ImageConverter.java
@@ -38,15 +38,8 @@ public class ImageConverter implements Converter {
     public void save(String absolutePath, String fileName) throws IOException {
         Path path = Path.of(absolutePath, fileName + "." + type.getFileExtension());
 
-        if (type == ImageType.JPEG) {
-            BufferedImage outputImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
-            outputImage.createGraphics().drawImage(image, 0, 0, Color.WHITE, null);
-            ImageIO.write(outputImage, type.getFileExtension(), path.toFile());
-            return;
-        }
-
-        FileOutputStream outputStream = new FileOutputStream(path.toFile());
-        ImageIO.write(image, type.getFileExtension(), outputStream);
-        outputStream.close();
+        BufferedImage outputImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        outputImage.createGraphics().drawImage(image, 0, 0, Color.WHITE, null);
+        ImageIO.write(outputImage, type.getFileExtension(), path.toFile());
     }
 }
