7b98d501729a7fcc996e3411506dad51886060c5
Noah Füreder <noah.fuereder@gmail.com>
2023-06-09 20:55:01 +0200

    don't your dare to override

--- src/main/java/converter/ImageConverter.java
+++ src/main/java/converter/ImageConverter.java
@@ -36,10 +36,27 @@ public class ImageConverter implements Converter {
      */
     @Override
     public void save(String absolutePath, String fileName) throws IOException {
-        Path path = Path.of(absolutePath, fileName + "." + type.getFileExtension());
+        Path path = getValidPath(Path.of(absolutePath, fileName + "." + type.getFileExtension()));
 
         BufferedImage outputImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
         outputImage.createGraphics().drawImage(image, 0, 0, Color.WHITE, null);
+
         ImageIO.write(outputImage, type.getFileExtension(), path.toFile());
+    }
+
+    /**
+     * Returns the valid path for the image.
+     * If the file name already exists, _x will be attached. x stands for a number.
+     *
+     * @param path for the image which should be saved
+     * @return valid path for the image
+     */
+    private static Path getValidPath(Path path) {
+        int x = 1;
+        while (path.toFile().exists()) {
+            path = Path.of(path.toFile().getParent() + "/" + path.toFile().getName().replace(".", "_" + x + "."));
+        }
+
+        return path;
     }
 }
